252c45ba6c32bc790dfc70ddd873404679aa27df
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2024-03-17 12:00:48 +0300

    feat: activate the first api

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drinks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'drinks'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
--- drinks/drinks/urls.py
+++ drinks/drinks/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for drinks project.
+"""drinks URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drinks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('drinks/', views.drink_list),
 ]
