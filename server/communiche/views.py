f3e8d35fcf06f99b8278fb22a3da8931b6ceb54a
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2024-03-19 14:08:28 +0300

    feat: add get and post functionality over User model

--- server/communiche/views.py
+++ server/communiche/views.py
@@ -1,12 +1,23 @@
 from django.http import JsonResponse
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def user_list(request):
     # get all the users
     # serialize them
     # return json
 
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
