ded65625a4aa95dd5c577ddb09970ed6a350f35f
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2024-04-12 10:04:45 +0300

    Add IndividualCommunity component to display community information

---
+++ client/src/views/community/components/IndividualCommunity.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function IndividualCommunity() {
+    return (
+        <div className="community-card mb-2">
+            <p className="text-white font-bold">Community Name</p>
+            <p className="inline-flex">100 Members</p>
+            <p className="inline-flex ml-2">10 Posts</p>
+            <p className="italic mb-4">Recent activity: 22.10.2023</p>
+        </div>
+    )
+}
--- client/src/views/community/components/RecentCommunities.tsx
+++ client/src/views/community/components/RecentCommunities.tsx
@@ -1,26 +1,18 @@
-import React from 'react'
 import { Card } from '@/components/ui'
+import IndividualCommunity from './IndividualCommunity'
 
 export default function RecentCommunities() {
     return (
         <div>
             <h3>Recent Communities</h3>
-            <Card className="mt-3">
-                <div className="community-card border-1 border-slate-500 mt-1">
-                    <p>Community Name</p>
-                    <p>Members: 100</p>
-                    <p>Posts</p>
-                </div>
-                <div className="community-card">
-                    <p>Community Name</p>
-                    <p>Members: 100</p>
-                    <p>Posts</p>
-                </div>
-                <div className="community-card">
-                    <p>Community Name</p>
-                    <p>Members: 100</p>
-                    <p>Posts</p>
-                </div>
+            <Card className="mt-3 h-75 overflow-auto">
+                <IndividualCommunity />
+                <IndividualCommunity />
+                <IndividualCommunity />
+                <IndividualCommunity />
+                <IndividualCommunity />
+                <IndividualCommunity />
+                <IndividualCommunity />
             </Card>
         </div>
     )
