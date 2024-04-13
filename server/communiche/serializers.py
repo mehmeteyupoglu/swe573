13092efb0d695234fe6ef409c06b7b025b881c8e
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2024-04-13 11:50:14 +0300

    Refactor serializers.py to include templates in CommunitySerializer

--- server/communiche/serializers.py
+++ server/communiche/serializers.py
@@ -1,17 +1,24 @@
 from rest_framework import serializers
-from .models import Template, User, Community
+from .models import Template, User, Community, CommunityTemplates
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'firstname', 'lastname', 'username', 'password', 'email', 'dob', 'country', 'phone', 'short_bio']
 
-class CommunitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Community
-        fields = ['id', 'name', 'description', 'created_at', 'updated_at', 'is_public', 'reputation_rating']
-
 class TemplateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Template
         fields = ['id', 'name', 'description', 'created_at', 'updated_at']
+
+class CommunitySerializer(serializers.ModelSerializer):
+    templates = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Community
+        fields = ['id', 'name', 'description', 'created_at', 'updated_at', 'is_public', 'reputation_rating', 'templates']
+
+    def get_templates(self, obj):
+        community_templates = CommunityTemplates.objects.filter(community=obj)
+        templates = [ct.template for ct in community_templates]
+        return TemplateSerializer(templates, many=True).data
