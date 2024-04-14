1a2457705e77b925c0ae131f6c60a24a5cf0239e
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-04-14 15:10:05 +0300

    Update AddTemplateForm component to include custom select option in CommunitySpecificTemplates.tsx

--- client/src/views/community/components/AddTemplateForm.tsx
+++ client/src/views/community/components/AddTemplateForm.tsx
@@ -2,12 +2,43 @@ import React, { useEffect, useState } from 'react'
 import Input from '@/components/ui/Input'
 import Button from '@/components/ui/Button'
 import Checkbox from '@/components/ui/Checkbox'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, FieldProps } from 'formik'
 import { FormContainer, FormItem } from '@/components/ui/Form'
 import * as Yup from 'yup'
 import { apiGetDataTypes } from '@/services/CommunityService'
 import { DataTypeResponse } from '@/@types/community'
 import { use } from 'i18next'
+import { Select } from '@/components/ui'
+
+import { HiCheck } from 'react-icons/hi' // import the check icon
+import { OptionProps } from 'react-select' // import the OptionProps type
+
+type DataTypeOption = {
+    value: string
+    label: string
+}
+
+const CustomSelectOption = ({
+    innerProps,
+    label,
+    isSelected,
+}: OptionProps<DataTypeOption>) => {
+    return (
+        <div
+            className={`flex items-center justify-between p-2 ${
+                isSelected
+                    ? 'bg-gray-100 dark:bg-gray-500'
+                    : 'hover:bg-gray-50 dark:hover:bg-gray-600'
+            }`}
+            {...innerProps}
+        >
+            <div className="flex items-center">
+                <span className="ml-2 rtl:mr-2">{label}</span>
+            </div>
+            {isSelected && <HiCheck className="text-emerald-500 text-xl" />}
+        </div>
+    )
+}
 
 export default function AddTemplateForm() {
     const validationSchema = Yup.object().shape({
@@ -27,6 +58,12 @@ export default function AddTemplateForm() {
             | 'File'
         )[]
     >([])
+
+    console.log(dataTypes)
+    const options: DataTypeOption[] = dataTypes?.map((dataType) => ({
+        value: dataType,
+        label: dataType,
+    }))
 
     useEffect(() => {
         const fetchDataType = async () => {
@@ -107,6 +144,31 @@ export default function AddTemplateForm() {
                                     component={Input}
                                     border={false}
                                 />
+                            </FormItem>
+                            <FormItem>
+                                <Field name="dataType">
+                                    {({ field, form }: FieldProps) => (
+                                        <Select<DataTypeOption>
+                                            field={field}
+                                            form={form}
+                                            options={options}
+                                            placeholder={'Select a data type'}
+                                            components={{
+                                                Option: CustomSelectOption,
+                                            }}
+                                            value={options.find(
+                                                (option) =>
+                                                    option.value === field.value
+                                            )}
+                                            onChange={(option) =>
+                                                form.setFieldValue(
+                                                    field.name,
+                                                    option?.value
+                                                )
+                                            }
+                                        />
+                                    )}
+                                </Field>
                             </FormItem>
                             <Button
                                 block
