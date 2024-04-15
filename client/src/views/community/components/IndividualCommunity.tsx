de7be3608494df569bb124c33214205b7d95e41c
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-04-15 10:06:33 +0300

    Refactor community detail view to use formatDate helper function in IndividualCommunity.tsx

--- client/src/views/community/components/IndividualCommunity.tsx
+++ client/src/views/community/components/IndividualCommunity.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import dayjs from 'dayjs'
+import { formatDate } from '@/utils/helpers'
 
 export default function IndividualCommunity({
     community,
@@ -10,14 +10,14 @@ export default function IndividualCommunity({
     members: number
     recentActivity: string
 }) {
-    const formattedDate = dayjs(recentActivity).format('MMMM DD, YYYY')
-
     return (
         <div className="community-card mb-2">
             <p className="text-white font-bold">{community}</p>
             <p className="inline-flex">{members} Members</p>
             <p className="inline-flex ml-2">10 Posts</p>
-            <p className="italic mb-4">Recent activity: {formattedDate}</p>
+            <p className="italic mb-4">
+                Recent activity: {formatDate(recentActivity)}
+            </p>
         </div>
     )
 }
