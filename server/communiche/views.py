164ed4854bda0eae1d06696c688d7a543ac3545c
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-04-16 17:04:42 +0300

    Refactor join_community and leave_community functions in views.py

--- server/communiche/views.py
+++ server/communiche/views.py
@@ -222,16 +222,16 @@ def join_community(request, community_id, user_id):
     except (Community.DoesNotExist, User.DoesNotExist):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if not community.is_public:
-        # Register join request
-        join_request = JoinRequest(community=community, user=user)
-        join_request.save()
-        return Response(status=status.HTTP_200_OK)
-    else:
-        community.members.add(user)
-        community.updated_at = datetime.now()  # Update the updated_at attribute
-        community.save()  # Save the updated community
-        return Response(status=status.HTTP_200_OK)
+    # if not community.is_public:
+    #     # Register join request
+    #     join_request = JoinRequest(community=community, user=user)
+    #     join_request.save()
+    #     return Response(status=status.HTTP_200_OK)
+    # else:
+    community.members.add(user)
+    community.updated_at = datetime.now()  # Update the updated_at attribute
+    community.save()  # Save the updated community
+    return Response(status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def leave_community(request, community_id, user_id):
@@ -246,11 +246,11 @@ def leave_community(request, community_id, user_id):
         community.updated_at = datetime.now()  # Update the updated_at attribute
         community.save()  # Save the updated community
 
-        if not community.is_public:
-            # Remove the join request record if it exists
-            join_request = JoinRequest.objects.filter(community=community, user=user).first()
-            if join_request:
-                join_request.delete()
+        # if not community.is_public:
+        #     # Remove the join request record if it exists
+        #     join_request = JoinRequest.objects.filter(community=community, user=user).first()
+        #     if join_request:
+        #         join_request.delete()
 
         return Response(status=status.HTTP_200_OK)
     else:
