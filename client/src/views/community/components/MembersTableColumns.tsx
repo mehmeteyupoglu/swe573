674d0305d1ecccaefe0445cdffce5a7e85d083ce
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-04-22 15:22:24 +0300

    add assign role btn

--- client/src/views/community/components/MembersTableColumns.tsx
+++ client/src/views/community/components/MembersTableColumns.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@/components/ui'
 import { formatDate, mapRoleToLabel } from '@/utils/helpers'
 import { ColumnDef } from '@tanstack/react-table'
+import React from 'react'
 
 const columns: ColumnDef<any>[] = [
     {
@@ -33,6 +35,27 @@ const columns: ColumnDef<any>[] = [
             const row = props.row.original
             const formattedDate = formatDate(row.joined_at)
             return <div className="flex">{formattedDate}</div>
+        },
+    },
+    {
+        header: 'Actions',
+        accessorKey: 'actions',
+        cell: (props) => {
+            const row = props.row.original
+
+            if (row.role != 1) {
+                return (
+                    <Button
+                        // disabled={is_owner}
+                        className="bg-blue-500 text-white"
+                        size="sm"
+                        variant="solid"
+                        onClick={() => console.log({ row })}
+                    >
+                        Assign Moderator
+                    </Button>
+                )
+            }
         },
     },
 ]
