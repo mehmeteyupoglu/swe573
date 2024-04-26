be3f11214e0e7c5832f39c7a88ddeb1cfd857bf9
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-04-26 16:30:09 +0300

    Refactor Post component to fetch and display members in a community

--- client/src/views/post/Post.tsx
+++ client/src/views/post/Post.tsx
@@ -1,5 +1,32 @@
-import React from 'react'
+import { Member } from '@/@types/community'
+import { apiFetchMembers } from '@/services/CommunityService'
+import { useAppSelector } from '@/store'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Post() {
-    return <div>Post</div>
+    const [members, setMembers] = useState<Member[]>([])
+    const fetchTrigger = useAppSelector(
+        (state) => state.community.community.fetchTrigger
+    )
+
+    const { id } = useParams<{ id: string }>()
+
+    useEffect(() => {
+        const fetchMembers = async () => {
+            try {
+                const members = await apiFetchMembers(String(id) ?? '')
+                if (members.status === 200) {
+                    setMembers(members.data as Member[])
+                }
+                // fetch members data
+                console.log('fetching members')
+            } catch (error) {
+                console.error('Error fetching members', error)
+            }
+        }
+
+        fetchMembers()
+    }, [fetchTrigger])
+    return <div></div>
 }
