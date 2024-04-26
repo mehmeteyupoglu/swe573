1d6b9725b8936a5b7813b69730499e11faabb541
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-04-26 20:02:02 +0300

    Refactor MapTemplates component to include template selection in MapTemplates.tsx

--- client/src/views/post/components/MapTemplates.tsx
+++ client/src/views/post/components/MapTemplates.tsx
@@ -1,12 +1,33 @@
 import { TemplateResponse, TemplateType } from '@/@types/community'
-import React from 'react'
+import React, { useEffect } from 'react'
 import MapFields from './MapFields'
+import { Select } from '@/components/ui'
+import { CustomSelectOption } from '@/components/shared/CustomSelectOption'
+import { CommonSelectOptionType } from '@/@types/common'
 
 export default function MapTemplates({
     templates,
 }: {
     templates: TemplateResponse[]
 }) {
+    const [template, setTemplate] = React.useState<string>('')
+    const [_templates, setTemplates] = React.useState<CommonSelectOptionType[]>(
+        []
+    )
+
+    React.useEffect(() => {
+        const mapTemplates = templates.map((template) => ({
+            value: template.template.name,
+            label: template.template.name,
+            id: template.id.toString(), // Convert the id to string
+        }))
+        setTemplates(mapTemplates)
+    }, [templates])
+
+    useEffect(() => {
+        console.log('Template:', template)
+    }, [template])
+
     return (
         <div>
             {templates.map((template) => (
@@ -20,6 +41,27 @@ export default function MapTemplates({
                     <MapFields fields={template.template.fields} />
                 </div>
             ))}
+
+            <Select
+                options={_templates}
+                placeholder={'Templates'}
+                components={{
+                    Option: CustomSelectOption,
+                }}
+                className="mb-4 max-w-md md:w-52"
+                value={
+                    _templates.find((option) => option.id === template) || null
+                }
+                onChange={(selectedOption) => {
+                    if (selectedOption) {
+                        const value = (selectedOption as CommonSelectOptionType)
+                            .id
+                        setTemplate(value) // Update the category query state
+                    } else {
+                        setTemplate('') // Clear the category query state
+                    }
+                }}
+            />
         </div>
     )
 }
