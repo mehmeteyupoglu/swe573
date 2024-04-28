1726133b226a2174eab215945643f8f57004fe45
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-04-28 22:09:32 +0300

    Update Home component to fetch and display posts

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,14 +1,17 @@
+import useFetchData from '@/utils/hooks/useFetchData'
 import RecentCommunities from './community/components/RecentCommunities'
 import DisplayPost from './post/components/DisplayPost'
+import { apiGetPosts } from '@/services/PostService'
 
 const Home = () => {
+    const data = useFetchData(apiGetPosts, [])
     return (
         <div className="flex gap-5">
             <div className="grow">
                 <h3>Hot Topics</h3>
-                <DisplayPost />
-                <DisplayPost />
-                <DisplayPost />
+                {(data?.data as any[])?.map((post: any) => (
+                    <DisplayPost key={post.id} post={post} />
+                ))}
             </div>
             <RecentCommunities />
         </div>
--- client/src/views/post/components/DisplayPost.tsx
+++ client/src/views/post/components/DisplayPost.tsx
@@ -1,23 +1,30 @@
 import { PostData } from '@/@types/post'
 import { Card } from '@/components/ui'
+import { formatDate } from '@/utils/helpers'
 import React from 'react'
 import { HiUserGroup } from 'react-icons/hi'
 
 export default function DisplayPost({ post }: { post: PostData }) {
+    const { username, firstname, lastname, content, community, created_at } =
+        post
     return (
         <Card className="mt-3">
             <div className="header justify-between">
-                <h3>Post Title</h3>
+                <h3>{content[0].field_value}</h3>
                 <div className="flex items-center">
-                    <p className="mr-3">Community</p>
+                    <p className="mr-3">{community.name}</p>
                     <HiUserGroup />
                 </div>
             </div>
             <div className="body mt-5 mb-5">
-                <p>Post Content Post Content Post Content Post Content</p>
+                <p>{content[1].field_value}</p>
             </div>
             <div className="footer flex justify-between">
-                <p>Posted by user x at y</p>
+                <p>
+                    Posted by
+                    <span>{firstname + ' ' + lastname}</span> at{' '}
+                    <span> {formatDate(created_at, 'MMMM DD, YYYY')}</span>
+                </p>
                 <div className="flex ">
                     <p>Comments (3)</p>
                     <p className="ml-3">Upvote (8) </p>
