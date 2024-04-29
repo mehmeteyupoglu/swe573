94b0d3ee679540a149207aff691f623b5f8a82f8
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-04-29 11:41:06 +0300

    Add search functionality to IngredientTableSearch component

--- client/src/views/Search.tsx
+++ client/src/views/Search.tsx
@@ -1,5 +1,22 @@
+import { useRef } from 'react'
+import IngredientTableSearch from './account/Settings/components/Ingredients/components/IngredientTableSearch'
+
 const Search = () => {
-    return <div className=""></div>
+    const inputRef = useRef<HTMLInputElement>(null)
+    // const tableData = useAppSelector((state) => state.crmMeals.data.tableData)
+
+    const handleInputChange = (val: string) => {
+        console.log('val', val)
+        return
+    }
+    return (
+        <div className="">
+            <IngredientTableSearch
+                ref={inputRef}
+                onInputChange={handleInputChange}
+            />
+        </div>
+    )
 }
 
 export default Search
