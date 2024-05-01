e67eae476137bd3e87a3bd01c04e0d641e2018ce
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-05-01 23:37:52 +0300

    Update gunicorn bind address to localhost in gunicorn_config.py

--- server/conf/gunicorn_config.py
+++ server/conf/gunicorn_config.py
@@ -1,4 +1,4 @@
 command = "/home/ubuntu/Swe573-eyupoglu/server/venv/bin/gunicorn"
 pythonpath = "/home/ubuntu/Swe573-eyupoglu/server"
-bind = "13.49.73.218:8000"
+bind = "127.0.0.1:8000"
 workers = 3
