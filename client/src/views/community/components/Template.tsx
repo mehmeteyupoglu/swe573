f09681f7e67bea6090e87ce9fb69b698a1943fd1
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-05-10 17:42:49 +0300

    Refactor community field components to improve consistency and clarity

--- client/src/views/community/components/Template.tsx
+++ client/src/views/community/components/Template.tsx
@@ -1,30 +1,12 @@
-import React from 'react'
 import { Card } from '@/components/ui'
-import { toSentenceCase } from '@/utils/helpers'
 import { TemplateType } from '@/@types/community'
+import Field from './Field'
 
 export default function Template({ template }: { template: TemplateType }) {
     return (
         <Card bordered className="mt-5">
             <div className="text-gray font-bold">{template.name}:</div>
-            <div className="flex underline text-gray font-bold mt-2">
-                <div className="mr-2 w-1/6">Field Name</div>
-                <div className="mr-2 w-1/6">Field Type</div>
-                <div>Required</div>
-            </div>
-            {template?.fields.map((field, index) => (
-                <div key={index} className="flex">
-                    <div className="mr-2 w-1/6">
-                        {toSentenceCase(field.field_name)}
-                    </div>
-                    <div className="mr-2 w-1/6">
-                        {toSentenceCase(field.field_type)}
-                    </div>
-                    <div className="mr-2 w-1/6">
-                        {field.isRequired ? 'Yes' : 'No'}
-                    </div>
-                </div>
-            ))}
+            <Field fields={template.fields} />
         </Card>
     )
 }
