01be6fd0d358a97cb9313b1c433ee20be59558bf
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-05-11 00:52:23 +0300

    Refactor user profile page to include user's communities and posts

--- client/src/views/profile/Profile.tsx
+++ client/src/views/profile/Profile.tsx
@@ -1,9 +1,8 @@
-import { Post } from '@/@types/user'
+import { Community, Post } from '@/@types/user'
 import { ActionLink } from '@/components/shared'
 import { apiGetUserInformation } from '@/services/UserService'
 import useFetchData from '@/utils/hooks/useFetchData'
 import { AxiosResponse } from 'axios'
-import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 type CustomerInfoFieldProps = {
@@ -18,16 +17,6 @@ const CustomerInfoField = ({ title, value }: CustomerInfoFieldProps) => {
             <p className="text-gray-700 dark:text-gray-200 font-semibold">
                 {!!value ? value : ' - '}
             </p>
-        </div>
-    )
-}
-
-const MapPost = ({ post }: { post: Post }) => {
-    return (
-        <div>
-            <ActionLink to={`/post/${post.id}`}>
-                {JSON.parse(post.content)[0].field_value}
-            </ActionLink>
         </div>
     )
 }
@@ -59,8 +48,29 @@ export default function Profile() {
                     <span>Posts</span>
                     <div>
                         {userInfo?.data?.posts.map((post: Post) => (
-                            <MapPost key={post.id} post={post} />
+                            <div>
+                                <ActionLink to={`/post/${post.id}`}>
+                                    {JSON.parse(post.content)[0].field_value}
+                                </ActionLink>
+                            </div>
                         ))}
+                    </div>
+                </div>
+
+                <div>
+                    <span>Communities</span>
+                    <div>
+                        {userInfo?.data?.communities.map(
+                            (community: Community) => (
+                                <div>
+                                    <ActionLink
+                                        to={`/community/${community.id}/details`}
+                                    >
+                                        {community.name}
+                                    </ActionLink>
+                                </div>
+                            )
+                        )}
                     </div>
                 </div>
             </div>
