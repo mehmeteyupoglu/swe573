e9ca35403a48e9737219b88ba4941b476d15b2ce
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-05-18 20:14:37 +0300

    Refactor Post component to conditionally render post content

--- client/src/views/search/components/Post.tsx
+++ client/src/views/search/components/Post.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 export default function Post({ post }: { post: PostData }) {
     return (
         <div>
-            {
+            {post.content && (
                 <>
-                    post.content && <div>{post.content[0].field_value}</div>
+                    <div>{post.content[0].field_value}</div>
                     <div>{post.content[1].field_value}</div>
                 </>
-            }
+            )}
         </div>
     )
 }
