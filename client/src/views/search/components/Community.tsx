2862fcfb812d6b898f4ba4de843ed753d0d18262
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2024-05-18 19:15:09 +0300

    Refactor Community component and remove console.log statement

--- client/src/views/search/components/Community.tsx
+++ client/src/views/search/components/Community.tsx
@@ -1,7 +1,6 @@
 import { CommunityType } from '@/@types/community'
 
 export default function Community({ community }: { community: CommunityType }) {
-    console.log({ community })
     return (
         <div>
             {community.name} - {community.description}
